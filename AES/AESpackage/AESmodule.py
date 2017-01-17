bd4f24c416b87da13e0041bda8ae4c731d9100a3
Anthony Constantino <aconstantino@neovest.com>
2017-01-17 11:48:14 -0700

    AES Project Setup

---
+++ AES/AESpackage/AESmodule.py
@@ -0,0 +1,56 @@
+'''
+Created on Jan 17, 2017
+
+@author: aconstantino
+'''
+
+import sys
+import io
+
+def subWord(word):
+    print('')
+    
+def rotateWord(word):
+    print('')
+    
+def keyExpansion():
+    print('')
+    
+def subBytes():
+    print('')
+
+def invertSubByte():
+    print('')
+
+def shiftRows():
+    print('')
+    
+def invertShiftRows():
+    print('')
+    
+def bitShifts(num):
+    print('')
+
+def finiteFieldAdd(num1, num2):
+    print('')
+    
+def finiteFieldMultiply(num1, num2):
+    print('')
+
+def mixColumns():
+    print('')
+    
+def invertMixColumns():
+    print('')
+    
+def addRoundKey(roundNum):
+    print('')
+
+def cipher():
+    print('')
+    
+def invertCipher():
+    print('')
+
+if __name__ == '__main__':
+    pass
